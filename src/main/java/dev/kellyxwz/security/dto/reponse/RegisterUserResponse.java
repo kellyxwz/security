86096029828ecadb7e6257bd3f6a7deeb06b4c3f
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-05 21:46:56 -0300

    adicionando um contrutor ao RegisterLoginResponse

--- src/main/java/dev/kellyxwz/security/dto/reponse/RegisterUserResponse.java
+++ src/main/java/dev/kellyxwz/security/dto/reponse/RegisterUserResponse.java
@@ -1,7 +1,15 @@
 package dev.kellyxwz.security.dto.reponse;
 
+import dev.kellyxwz.security.entity.User;
+
 public record RegisterUserResponse(
         String name,
         String email
 ) {
+    public RegisterUserResponse(User user) {
+        this(
+                user.getName(),
+                user.getEmail()
+        );
+    }
 }
