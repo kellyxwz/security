9c16b731b2bb84a271f6d2accc35d4807bc2ae43
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-05 21:09:57 -0300

    criado o dto de RegisterUserRequest

--- src/main/java/dev/kellyxwz/security/dto/request/RegisterUserRequest.java
+++ src/main/java/dev/kellyxwz/security/dto/request/RegisterUserRequest.java
@@ -2,13 +2,12 @@ package dev.kellyxwz.security.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record LoginRequest(
-
+public record RegisterUserRequest (
+        @NotEmpty(message = "nome é obrigatório")
+        String name,
         @NotEmpty(message = "email é obrigatório")
         String email,
         @NotEmpty(message = "senha é obrigatório" )
         String password
-) {
-
-
+){
 }
